58634ad8487b488e18132cd2dcd3af20ef69e4a6
Fernando Martins <fernandomartins996@gmail.com>
2018-11-17 12:12:55 +0000

    Testes iniciais, load data.

---
+++ ANN_Assignment_1.m
@@ -0,0 +1,60 @@
+clc
+clear all
+
+%Apneatic Patient
+for i=1:1:37
+    if i< 10
+        name = strcat("APN00" ,int2str(i), ".mat");
+    else
+        name = strcat("APN0" , int2str(i) , ".mat");
+    end
+    %Load Data
+    load(strcat('HuGCDN2014 DATABASE\RR\', name))
+    %Load Annotation -> salida_man
+    load(strcat('HuGCDN2014 DATABASE\LABELS\', name))
+    
+     for j=3:1:size(RR_notch_abs_pr_ada, 2)-3
+         DataAPN{i,j-2} = RR_notch_abs_pr_ada(j);
+         AnoAPN{i,j-2} = salida_man_1m(j);
+     end
+end
+
+%Normal Patient
+for i=1:1:40
+    if i< 10
+        name = strcat("CON00" ,int2str(i), ".mat");
+    else
+        name = strcat("CON0" , int2str(i) , ".mat");
+    end
+    %Load Data
+    load(strcat('HuGCDN2014 DATABASE\RR\', name))
+    %Load Annotation -> salida_man
+    load(strcat('HuGCDN2014 DATABASE\LABELS\', name))
+    
+     for j=3:1:size(RR_notch_abs_pr_ada, 2)-3
+         DataCON{i,j-2} = RR_notch_abs_pr_ada(j);
+         AnoCON{i,j-2} = salida_man_1m(j);
+     end
+end
+
+%trainDataSet = [DataCON(1,20),DataAPN(1,20)]
+
+
+x= DataAPN([1,20], :)
+
+trainDataSet = [DataAPN;DataAPN]
+
+
+
+%trainDataSet = append(DataCON(1,20),DataAPN(1,20))
+
+
+
+trainDataSetAno = [AnoCON(1,20);AnoAPN(1,20)]
+
+
+
+%  for i=3:1:size(RR_notch_abs_pr_ada, 2)-3
+%      DataAPN(i-2) = RR_notch_abs_pr_ada(i);
+%      AnoAPN(i-2) = salida_man_1m(i);
+% end
