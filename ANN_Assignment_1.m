9957fcd04aeb1c7e482affed36b2e63c492e5614
Fernando Martins <fernandomartins996@gmail.com>
2018-11-30 22:47:59 +0000

    Ta quase lá

--- ANN_Assignment_1.m
+++ ANN_Assignment_1.m
@@ -182,83 +182,130 @@ ano_train = [ LearnANO30Percent LearnANO70Percent];
 %==========================================================================
 clear TestANO LearnData70Percent LearnData30Percent LearnANO30Percent LearnANO70Percent asd aux cc col cvNN M maxVal minVal RMSSD S V TestData LearnData cc_max cc_med cc_min i j name RR_notch_abs_pr_ada salida_man salida_man_1m;
 
+%==========================================================================
+%              create ANN check the performance 1st fold                  =
+%==========================================================================
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% create ANN check the performance 1st fold
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+aux_train_data_size = size(feature_train);  %All the trainData
+aux_train_size = size(feature_train_70_percent); % 70% of the trainData
+aux_validation_size = size(feature_train_30_percent);  % 30% of the trainData
 
-trainFcn = 'trainscg';  % Scaled conjugate gradient backpropagation.
-
-% Create a Pattern Recognition Network
-hiddenLayerSize = 500;
-net = patternnet(hiddenLayerSize, trainFcn);
-
-%[trainInd,valInd,testInd] = ...
-%divideind(3000,1:2000,2001:2500,2501:3000);
-
-% Setup Division of Data for Training, Validation, Testing
-net.divideFcn = ''; 
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 30/100;
-net.divideParam.testRatio = 0/100;
-net.performFcn = 'crossentropy';
-net.trainParam.epochs = 50;
-net.trainParam.max_fail=501;
-
-% Train the Network
-[net,tr] = train(net,feature_train,ano_train);
+train_data_size = aux_train_data_size(2) %Tamanho total da linha com pacientes
+train_size = aux_train_size(2) %Primeiros 70% dos pacientes
+validation_size = aux_validation_size(2) %30% dos pacientes
+[trainInd,valInd,testInd] = divideind(train_data_size, 1:train_size,  train_size+1:train_size+validation_size); 
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% create ANN check the performance 2st fold
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+trs1_best_perf = [];
+trs_test1_best_perf = [];
+trs1_best_vperf = [];
+trs_test1_best_vperf = [];
 
-clear net
+nets_test2_best_perf = [];
+trs2_best_perf = [];
 
-trainFcn = 'trainscg';  % Scaled conjugate gradient backpropagation.
-
-% Create a Pattern Recognition Network
-hiddenLayerSize = 500;
-net = patternnet(hiddenLayerSize, trainFcn);
-
-% Setup Division of Data for Training, Validation, Testing
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 30/100;
-net.divideParam.testRatio = 0/100;
-net.performFcn = 'crossentropy';
-net.trainParam.epochs = 50;
-net.trainParam.max_fail=501;
-
-
-% Train the Network
-[net_test,tr_test] = train(net,feature_test,ano_test);
-[net3,tr3] = train(net,feature_train,ano_train);
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Make the average result from both folds
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+nets_test2_best_vperf = [];
+trs2_best_vperf = [];
 
 
 
+for i=1:10
+    
+    clear net
+    trainFcn = 'trainscg';  % Scaled conjugate gradient backpropagation.
+    % Create a Pattern Recognition Network
+    hiddenLayerSize = 500;
+    net = patternnet(hiddenLayerSize, trainFcn);
+    net.performFcn = 'crossentropy';
+    
+    net.divideFcn = 'divideind';
+    %net.trainParam.showWindow = false;   
+    net.divideParam.trainInd=trainInd;
+    net.divideParam.valInd=valInd;
+    %net.divideParam.testInd=testInd;
 
-% % Test the Network
-% y = net(x);
-% e = gsubtract(t,y);
-% performance = perform(net,t,y)
-% tind = vec2ind(t);
-% yind = vec2ind(y);
-% percentErrors = sum(tind ~= yind)/numel(tind);
+    net.trainParam.epochs = 10;
+    net.trainParam.max_fail=501;
+
+    % Train the Network
+    [nets1{i},trs1{i}] = train(net,feature_train,ano_train);
+    [nets_test1{i},trs_test1{i}] = train(net,feature_test,ano_test);
+    
+    trs1_best_perf(i) = trs1{i}.best_perf;
+    trs_test1_best_perf(i) = trs_test1{i}.best_perf;
+    
+    trs1_best_vperf(i) = trs1{i}.best_vperf;
+    trs_test1_best_vperf(i) = trs1{i}.best_vperf;
+
+end
+
+%==========================================================================
+%              create ANN check the performance 2st fold                  =
+%==========================================================================
+for i=1:10 
+    
+    clear net
+    trainFcn = 'trainscg';  % Scaled conjugate gradient backpropagation.
+    % Create a Pattern Recognition Network
+    hiddenLayerSize = 500;
+    net = patternnet(hiddenLayerSize, trainFcn);
+    net.performFcn = 'crossentropy';
+    
+    net.divideFcn = 'divideind';
+    %net.trainParam.showWindow = false;   
+    net.divideParam.trainInd=trainInd;
+    net.divideParam.valInd=valInd;
+    %net.divideParam.testInd=testInd;
+
+    net.trainParam.epochs = 10;
+    net.trainParam.max_fail=501;
+    
+    % Train the Network
+    [nets_test2{i},trs_test2{i}] = train(net,feature_test,ano_test);
+    [nets2{i},trs2{i}] = train(net,feature_train,ano_train);
+    
+    trs_test2_best_perf(i) = trs_test2{i}.best_perf;
+    trs2_best_perf(i) = trs2{i}.best_perf;
+    
+    trs_test2_best_vperf(i) = trs_test2{i}.best_vperf;
+    trs2_best_vperf(i) = trs2{i}.best_vperf;
+
+end
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%% Make the average result from both folds
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+ media_best_perf =  [ mean(trs1_best_perf) mean(trs2_best_perf) mean(trs_test1_best_perf)  mean(trs_test2_best_perf)];  
+ media_best_vperf =  [ mean(trs1_best_vperf) mean(trs2_best_vperf) mean(trs_test1_best_vperf)  mean(trs_test2_best_vperf)]; 
+
+ min_best_perf =  [ min(trs1_best_perf) min(trs2_best_perf) min(trs_test1_best_perf)  min(trs_test2_best_perf)];  
+ min_best_vperf =  [ min(trs1_best_vperf) min(trs2_best_vperf) min(trs_test1_best_vperf)  min(trs_test2_best_vperf)] ;
+ 
+ max_best_perf =  [ max(trs1_best_perf) max(trs2_best_perf) max(trs_test1_best_perf)  max(trs_test2_best_perf)];  
+ max_best_vperf =  [ max(trs1_best_vperf) max(trs2_best_vperf) max(trs_test1_best_vperf)  max(trs_test2_best_vperf)] ;
+
+
+ 
+
+% % % Test the Network
+% % y = net(x);
+% % e = gsubtract(t,y);
+% % performance = perform(net,t,y)
+% % tind = vec2ind(t);
+% % yind = vec2ind(y);
+% % percentErrors = sum(tind ~= yind)/numel(tind);
+% % 
+% % % View the Network
+% % view(net)
 % 
-% % View the Network
-% view(net)
-
-% Plots
-% Uncomment these lines to enable various plots.
-%figure, plotperform(tr)
-%figure, plottrainstate(tr)
-%figure, ploterrhist(e)
-%figure, plotconfusion(t,y)
-%figure, plotroc(t,y)
-
-
+% % Plots
+% % Uncomment these lines to enable various plots.
+% %figure, plotperform(tr)
+% %figure, plottrainstate(tr)
+% %figure, ploterrhist(e)
+% %figure, plotconfusion(t,y)
+% %figure, plotroc(t,y)
+% 
+% 
