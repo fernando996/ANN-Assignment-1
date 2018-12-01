0675f2b815de93e506a0cf12a907dbe4402ed959
Fernando Martins <fernandomartins996@gmail.com>
2018-12-01 17:06:24 +0000

    Last Commit

--- ANN_Assignment_1.m
+++ ANN_Assignment_1.m
@@ -112,7 +112,12 @@ for col=1:size(LearnData30Percent,2)
     RMSSD(col)=sqrt(sum(((mean(asd)-asd).^2))/length(asd-1)); %Root Mean Square of the Successive Differences
 end
 
-feature_train_30_percent=[M' S' V' cvNN' RMSSD' minVal' maxVal']';
+feature_train_30_percent{1}=[M' S']';
+feature_train_30_percent{2}=[M' S' V' cvNN']';
+feature_train_30_percent{3}=[M' S' V' cvNN' RMSSD' minVal']';
+feature_train_30_percent{4}=[M' S' V' cvNN' RMSSD' minVal' maxVal' cc_max']';
+feature_train_30_percent{5}=[M' S' V' cvNN' RMSSD' minVal' maxVal' cc_max' cc_min' RMSSD']';
+
 ano_train_30_percent = LearnANO30Percent;
 
 
@@ -143,8 +148,13 @@ for col=1:size(LearnData70Percent,2)
     RMSSD(col)=sqrt(sum(((mean(asd)-asd).^2))/length(asd-1)); %Root Mean Square of the Successive Differences
 end
 
-feature_train_70_percent=[M' S' V' cvNN' RMSSD' minVal' maxVal']';
-ano_train_70_percent = LearnANO30Percent;
+feature_train_70_percent{1}=[M' S']';
+feature_train_70_percent{2}=[M' S' V' cvNN']';
+feature_train_70_percent{3}=[M' S' V' cvNN' RMSSD' minVal']';
+feature_train_70_percent{4}=[M' S' V' cvNN' RMSSD' minVal' maxVal' cc_max']';
+feature_train_70_percent{5}=[M' S' V' cvNN' RMSSD' minVal' maxVal' cc_max' cc_min' RMSSD']';
+
+ano_train_70_percent = LearnANO70Percent;
  
   
 %==========================================================================
@@ -171,10 +181,21 @@ for col=1:size(TestData,2)
     RMSSD(col)=sqrt(sum(((mean(asd)-asd).^2))/length(asd-1)); %Root Mean Square of the Successive Differences
 end
 
-feature_test=[M' S' V' cvNN' RMSSD' minVal' maxVal']';
+feature_test{1}=[M' S']';
+feature_test{2}=[M' S' V' cvNN']';
+feature_test{3}=[M' S' V' cvNN' RMSSD' minVal']';
+feature_test{4}=[M' S' V' cvNN' RMSSD' minVal' maxVal' cc_max']';
+feature_test{5}=[M' S' V' cvNN' RMSSD' minVal' maxVal' cc_max' cc_min' RMSSD']';
+
+
 ano_test = TestANO;
 
-feature_train = [ feature_train_30_percent feature_train_70_percent];
+feature_train{1} = [ feature_train_30_percent{1} feature_train_70_percent{1}];
+feature_train{2} = [ feature_train_30_percent{2} feature_train_70_percent{2}];
+feature_train{3} = [ feature_train_30_percent{3} feature_train_70_percent{3}];
+feature_train{4} = [ feature_train_30_percent{4} feature_train_70_percent{4}];
+feature_train{5} = [ feature_train_30_percent{5} feature_train_70_percent{5}];
+
 ano_train = [ LearnANO30Percent LearnANO70Percent];
 
 %==========================================================================
@@ -185,104 +206,114 @@ clear TestANO LearnData70Percent LearnData30Percent LearnANO30Percent LearnANO70
 %==========================================================================
 %              create ANN check the performance 1st fold                  =
 %==========================================================================
-
-aux_train_data_size = size(feature_train);  %All the trainData
-aux_train_size = size(feature_train_70_percent); % 70% of the trainData
-aux_validation_size = size(feature_train_30_percent);  % 30% of the trainData
+aux_train_data_size = size(feature_train{1,1});  %All the trainData
+aux_train_size = size(feature_train_70_percent{1,1}); % 70% of the trainData
+aux_validation_size = size(feature_train_30_percent{1,1});  % 30% of the trainData
 
 train_data_size = aux_train_data_size(2) %Tamanho total da linha com pacientes
 train_size = aux_train_size(2) %Primeiros 70% dos pacientes
 validation_size = aux_validation_size(2) %30% dos pacientes
 [trainInd,valInd,testInd] = divideind(train_data_size, 1:train_size,  train_size+1:train_size+validation_size); 
 
-for i=1:10
-    
-    clear net tr trainTargets valTargets testTargets;
-    trainFcn = 'trainscg';  % Scaled conjugate gradient backpropagation.
-    % Create a Pattern Recognition Network
-    hiddenLayerSize = 500;
-    net = patternnet(hiddenLayerSize, trainFcn);
-    net.performFcn = 'crossentropy';
-    
-    net.divideFcn = 'divideind';
-    %net.trainParam.showWindow = false;   
-    net.divideParam.trainInd=trainInd;
-    net.divideParam.valInd=valInd;
-    %net.divideParam.testInd=testInd;
+trainFcn = ["trainscg" "traingdx" "trainbfg"];
+neurons_number = [ 1 10 20 30 ];
+aux = 1 ;
 
-    net.trainParam.epochs = 10;
-    net.trainParam.max_fail=501;
-     
-    % Train the Network
-    [net tr ] = train(net,feature_train,ano_train);
-    nets1{i} = net;
-    trs1{i} =  tr;
-   
-    trs1_best_perf(i) = trs1{i}.best_perf;
-    trs1_best_vperf(i) = trs1{i}.best_vperf;
+for w=1:1:length(trainFcn)
+    for z=1:1:length(feature_train)
+        for j=1:1:length(neurons_number)
+            for i=1:10
+                clear net tr trainTargets valTargets testTargets;
+                % Create a Pattern Recognition Network
+                net = patternnet(neurons_number(j), char(trainFcn(w)));
+                net.performFcn = 'crossentropy';
 
-    % Test the Network
-    y = net(feature_test); 
-    s = net(feature_train); 
-    e1{i} = gsubtract(ano_test,y);
-    performance1{i} = perform(net,ano_test,y);
-    tind = vec2ind(ano_test);
-    yind = vec2ind(y);
-    percentErrors1{i} = sum(tind ~= yind)/numel(tind);
+                net.divideFcn = 'divideind';
+                %net.trainParam.showWindow = false;   
+                net.divideParam.trainInd=trainInd;
+                net.divideParam.valInd=valInd;
+                %net.divideParam.testInd=testInd;
 
-    % Recalculate Training, Validation and Test Performance
-    trainTargets = ano_train .* tr.trainMask{1};
-    valTargets = ano_train .* tr.valMask{1};
-    trainPerformance1(i) = perform(net,trainTargets,s);
-    valPerformance1(i) = perform(net,valTargets,s);
-    
+                net.trainParam.epochs = 10;
+                net.trainParam.max_fail=501;
+
+                % Train the Network
+                [net tr ] = train(net,feature_train{z},ano_train);
+                nets1{aux} = net;
+                trs1{aux} =  tr;
+
+                trs1_best_perf(aux) = trs1{aux}.best_perf;
+                trs1_best_vperf(aux) = trs1{aux}.best_vperf;
+
+                % Test the Network
+                y = net(feature_test{z}); 
+                s = net(feature_train{z}); 
+                e1{aux} = gsubtract(ano_test,y);
+                performance1{aux} = perform(net,ano_test,y);
+                tind = vec2ind(ano_test);
+                yind = vec2ind(y);
+                percentErrors1{aux} = sum(tind ~= yind)/numel(tind);
+
+                % Recalculate Training, Validation and Test Performance
+                trainTargets = ano_train .* tr.trainMask{1};
+                valTargets = ano_train .* tr.valMask{1};
+                trainPerformance1(aux) = perform(net,trainTargets,s);
+                valPerformance1(i) = perform(net,valTargets,s);
+                aux = aux + 1 
+            end
+
+        end
+    end
 end
 
 %==========================================================================
 %              create ANN check the performance 2st fold                  =
 %==========================================================================
+aux = 1 ;
+for w=1:1:length(trainFcn)
+    for z=1:1:length(feature_train)
+        for j=1:1:length(neurons_number)
+            for i=1:10
+                clear net tr trainTargets valTargets testTargets;
+                % Create a Pattern Recognition Network
+                net = patternnet(neurons_number(j), char(trainFcn(w)));
+                net.performFcn = 'crossentropy';
 
-for i=1:10
-    
-    clear net tr trainTargets valTargets testTargets;
-    trainFcn = 'trainscg';  % Scaled conjugate gradient backpropagation.
-    % Create a Pattern Recognition Network
-    hiddenLayerSize = 500;
-    net = patternnet(hiddenLayerSize, trainFcn);
-    net.performFcn = 'crossentropy';
-    
-    net.divideFcn = 'divideind';
-    %net.trainParam.showWindow = false;   
-    net.divideParam.trainInd=trainInd;
-    net.divideParam.valInd=valInd;
-    %net.divideParam.testInd=testInd;
+                net.divideFcn = 'divideind';
+                %net.trainParam.showWindow = false;   
+                net.divideParam.trainInd=trainInd;
+                net.divideParam.valInd=valInd;
+                %net.divideParam.testInd=testInd;
 
-    net.trainParam.epochs = 10;
-    net.trainParam.max_fail=501;
-     
-    % Train the Network
-    [net tr ] = train(net,feature_test,ano_test);
-    nets2{i} = net;
-    trs2{i} =  tr;
-   
-    trs2_best_perf(i) = trs2{i}.best_perf;
-    trs2_best_vperf(i) = trs2{i}.best_vperf;
+                net.trainParam.epochs = 10;
+                net.trainParam.max_fail=501;
 
-    % Test the Network
-    y = net(feature_train); 
-    s = net(feature_test); 
-    e2{i} = gsubtract(ano_train,y);
-    performance2{i} = perform(net,ano_train,y);
-    tind = vec2ind(ano_train);
-    yind = vec2ind(y);
-    percentErrors2{i} = sum(tind ~= yind)/numel(tind);
+                % Train the Network
+                [net tr ] = train(net,feature_test{z},ano_test);
+                nets2{aux} = net;
+                trs2{aux} =  tr;
 
-    % Recalculate Training, Validation and Test Performance
-    trainTargets = ano_test .* tr.trainMask{1};
-    valTargets = ano_test .* tr.valMask{1};
-    trainPerformance2(i) = perform(net,trainTargets,s);
-    valPerformance2(i) = perform(net,valTargets,s);
-    
+                trs2_best_perf(aux) = trs2{aux}.best_perf;
+                trs2_best_vperf(aux) = trs2{aux}.best_vperf;
+
+                % Test the Network
+                y = net(feature_train{z}); 
+                s = net(feature_test{z}); 
+                e2{aux} = gsubtract(ano_train,y);
+                performance2{aux} = perform(net,ano_train,y);
+                tind = vec2ind(ano_train);
+                yind = vec2ind(y);
+                percentErrors2{aux} = sum(tind ~= yind)/numel(tind);
+
+                % Recalculate Training, Validation and Test Performance
+                trainTargets = ano_test .* tr.trainMask{1};
+                valTargets = ano_test .* tr.valMask{1};
+                trainPerformance2(aux) = perform(net,trainTargets,s);
+                valPerformance2(aux) = perform(net,valTargets,s);
+                aux = aux +1 ;
+            end
+        end
+    end
 end
 
 
@@ -305,22 +336,144 @@ feature_test feature_train feature_train_30_percent feature_train_70_percent ...
  max_best_perf =  [ max(trs1_best_perf) max(trs2_best_perf)];  
  max_best_vperf =  [ max(trs1_best_vperf) max(trs2_best_vperf)] ;
  
+ 
+ 
+ %-------------------------------------------------------------------------------------------------%
  %Melhor primeira posição
- position1 = [ find(trs1_best_perf==min(trs1_best_perf)) find(trs2_best_perf==min(trs2_best_perf))];
+ aux_Position_FIND_TRS1_best_perf = find(trs1_best_perf==min(trs1_best_perf));
+ aux_actual_best_vperf = 9999;
+ aux_correct_position1=0;
  
+ %%%TR1 iguais
+ if( length(aux_Position_FIND_TRS1_best_perf) >1)
+     for i=1:1:length(aux_Position_FIND_TRS1_best_perf)
+         if aux_actual_best_vperf < trs1{aux_Position_FIND_TRS1_best_perf(i)}.best_vperf
+            aux_actual_best_vperf = trs1{aux_Position_FIND_TRS1_best_perf(i)}.best_vperf;
+            aux_correct_position1 = aux_Position_FIND_TRS1_best_perf(i);
+         elseif aux_correct_position1 == 0
+             aux_correct_position1 = aux_Position_FIND_TRS1_best_perf(i);
+         end      
+     end
+ else
+     aux_correct_position1 =  find(trs1_best_perf==min(trs1_best_perf));
+ end
+
+aux_Position_FIND_TRS2_best_perf = find(trs2_best_perf==min(trs2_best_perf));
+aux_actual_best_vperf = 9999;
+aux_correct_position2 = 0;
+    
+ %%%TR2 iguais
+if( length(aux_Position_FIND_TRS2_best_perf) >1)
+    for i=1:1:length(aux_Position_FIND_TRS2_best_perf)
+        if aux_actual_best_vperf < trs2{aux_Position_FIND_TRS2_best_perf(i)}.best_vperf
+           aux_actual_best_vperf = trs2{aux_Position_FIND_TRS2_best_perf(i)}.best_vperf;
+           aux_correct_position2 = aux_Position_FIND_TRS2_best_perf(i);
+         elseif aux_correct_position2 == 0
+             aux_correct_position2 = aux_Position_FIND_TRS2_best_perf(i);    
+        end      
+    end
+else
+    aux_correct_position2 =  find(trs2_best_perf==min(trs2_best_perf));
+end
+ 
+ position1 = [ aux_correct_position1 aux_correct_position2]
+ 
+ %--------------------------------------------------------------------------------------------------------%
+ 
+ 
+ %Melhor segunda posição
  aux_trs1_best_perf = trs1_best_perf;
- aux_trs2_best_perf = trs2_best_perf;
- 
+ aux_trs2_best_perf = trs2_best_perf; 
  aux_trs1_best_perf(position1(1)) = 9999;
  aux_trs2_best_perf(position1(2)) = 9999;
  
- position2 = [ find(aux_trs1_best_perf==min(aux_trs1_best_perf)) find(aux_trs2_best_perf==min(aux_trs2_best_perf))];
  
+ aux_Position_FIND_TRS1_best_perf = find(aux_trs1_best_perf==min(aux_trs1_best_perf));
+ aux_actual_best_vperf = 9999;
+ aux_correct_position1 = 0;
+ 
+ %%%TR1 iguais
+ if( length(aux_Position_FIND_TRS1_best_perf) >1)
+     for i=1:1:length(aux_Position_FIND_TRS1_best_perf)
+         if aux_actual_best_vperf < trs1{aux_Position_FIND_TRS1_best_perf(i)}.best_vperf
+            aux_actual_best_vperf = trs1{aux_Position_FIND_TRS1_best_perf(i)}.best_vperf;
+            aux_correct_position1 = aux_Position_FIND_TRS1_best_perf(i);
+          elseif aux_correct_position1 == 0
+             aux_correct_position1 = aux_Position_FIND_TRS1_best_perf(i);
+          end      
+     end
+ else
+     aux_correct_position1 =  find(aux_trs1_best_perf==min(aux_trs1_best_perf));
+ end
+ 
+aux_Position_FIND_TRS2_best_perf = find(aux_trs2_best_perf==min(aux_trs2_best_perf));
+aux_actual_best_vperf = 9999;
+aux_correct_position2 =0;
+    
+ %%%TR2 iguais
+if( length(aux_Position_FIND_TRS2_best_perf) >1)
+    for i=1:1:length(aux_Position_FIND_TRS2_best_perf)
+        if aux_actual_best_vperf < trs2{aux_Position_FIND_TRS2_best_perf(i)}.best_vperf
+           aux_actual_best_vperf = trs2{aux_Position_FIND_TRS2_best_perf(i)}.best_vperf;
+           aux_correct_position2 = aux_Position_FIND_TRS2_best_perf(i);
+        elseif aux_correct_position2 == 0
+             aux_correct_position2 = aux_Position_FIND_TRS2_best_perf(i);
+        end      
+    end
+else
+    aux_correct_position2 =  find(aux_trs2_best_perf==min(aux_trs2_best_perf));
+end
+
+ position2 = [ aux_correct_position1 aux_correct_position2];
+ 
+
+%--------------------------------------------------------------------------------------------------------%
+ 
+ 
+ %Melhor terceira posição
  aux_trs1_best_perf(position2(1)) = 9999;
  aux_trs2_best_perf(position2(2)) = 9999;
  
- position3 = [ find(aux_trs1_best_perf==min(aux_trs1_best_perf)) find(aux_trs2_best_perf==min(aux_trs2_best_perf))];
+ 
+ aux_Position_FIND_TRS1_best_perf = find(aux_trs1_best_perf==min(aux_trs1_best_perf));
+ aux_actual_best_vperf = 9999;
+ aux_correct_position1 = 0;
+ 
+ %%%TR1 iguais
+ if( length(aux_Position_FIND_TRS1_best_perf) >1)
+     for i=1:1:length(aux_Position_FIND_TRS1_best_perf)
+         if aux_actual_best_vperf < trs1{aux_Position_FIND_TRS1_best_perf(i)}.best_vperf
+            aux_actual_best_vperf = trs1{aux_Position_FIND_TRS1_best_perf(i)}.best_vperf;
+            aux_correct_position1 = aux_Position_FIND_TRS1_best_perf(i);
+         elseif aux_correct_position1 == 0
+             aux_correct_position1 = aux_Position_FIND_TRS1_best_perf(i);
+         end      
+     end
+ else
+     aux_correct_position1 =  find(aux_trs1_best_perf==min(aux_trs1_best_perf));
+ end
+ 
+aux_Position_FIND_TRS2_best_perf = find(aux_trs2_best_perf==min(aux_trs2_best_perf));
+aux_actual_best_vperf = 9999;
+aux_correct_position1 = 0;
+    
+ %%%TR2 iguais
+if( length(aux_Position_FIND_TRS2_best_perf) >1)
+    for i=1:1:length(aux_Position_FIND_TRS2_best_perf)
+        if aux_actual_best_vperf < trs2{aux_Position_FIND_TRS2_best_perf(i)}.best_vperf
+           aux_actual_best_vperf = trs2{aux_Position_FIND_TRS2_best_perf(i)}.best_vperf;
+           aux_correct_position2 = aux_Position_FIND_TRS2_best_perf(i);
+        elseif aux_correct_position1 == 0
+             aux_correct_position1 = aux_Position_FIND_TRS1_best_perf(i);
+        end      
+    end
+else
+    aux_correct_position2 =  find(aux_trs2_best_perf==min(aux_trs2_best_perf));
+end
+ 
+ position3 = [ aux_correct_position1 aux_correct_position2 ];
   
+ %Defining the best final 3 position! Of each network.
  position_trs1_best_perf = [position1(1) position2(1) position3(1)];
  position_trs2_best_perf = [position1(2) position2(2) position3(2)];
  
@@ -329,8 +482,8 @@ feature_test feature_train feature_train_30_percent feature_train_70_percent ...
  if min_best_perf(1)== min_best_perf(2) 
          aux_position_left = find(trs1_best_perf==min(trs1_best_perf))
          aux_position_rigth = find(trs2_best_perf==min(trs2_best_perf))
-         aux_trs1_best_vperf = trs1_best_perf{aux_position_left}.best_vperf;
-         aux_trs2_best_vperf = trs2_best_perf{aux_position_rigth}.best_vperf;
+         aux_trs1_best_vperf = trs1{aux_position_left}.best_vperf;
+         aux_trs2_best_vperf = trs2{aux_position_rigth}.best_vperf;
      if aux_trs1_best_vperf <= aux_trs2_best_vperf
         best_position = [aux_position_left 0];
      else
@@ -347,8 +500,8 @@ feature_test feature_train feature_train_30_percent feature_train_70_percent ...
 if  max_best_perf(1)==  max_best_perf(2) 
         aux_position_left = find( trs1_best_perf==max( trs1_best_perf))
         aux_position_rigth = find( trs2_best_perf==max( trs2_best_perf))
-        aux_trs1_worst_vperf = trs1_best_perf{aux_position_left}.best_vperf;
-        aux_trs2_worst_vperf = trs2_best_perf{aux_position_rigth}.best_vperf;
+        aux_trs1_worst_vperf = trs1{aux_position_left}.best_vperf;
+        aux_trs2_worst_vperf = trs2{aux_position_rigth}.best_vperf;
     if aux_trs1_worst_vperf >= aux_trs2_worst_vperf
        worst_position = [aux_position_left 0];
     else
@@ -361,7 +514,8 @@ else
 end
 
  clear position1 position2 position3 aux_trs1_best_perf aux_trs2_best_perf aux_position_left ...
-    aux_position_rigth aux_trs1_worst_vperf aux_trs2_worst_vperf ;
+    aux_position_rigth aux_trs1_worst_vperf aux_trs2_worst_vperf aux_correct_position2 aux_correct_position1 ...
+    aux_actual_best_perf aux_Position_FIND_TRS1_best_perf aux_Position_FIND_TRS2_best_perf;
 % disp(min(mse_val));
 % Position=find(mse_val==min(mse_val));
 % disp(Position);
